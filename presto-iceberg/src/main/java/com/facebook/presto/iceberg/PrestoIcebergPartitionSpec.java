0a272d6e6fc25e8b06b93d97240df9306f229f5d
Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
2024-07-08 21:49:00 +0530

    [iceberg] Add wrapper class PrestoIcebergPartitionSpec for org.apache.iceberg.PartitionSpec

---
+++ presto-iceberg/src/main/java/com/facebook/presto/iceberg/PrestoIcebergPartitionSpec.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.iceberg;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+public class PrestoIcebergPartitionSpec
+{
+    private final int specId;
+    private final PrestoIcebergSchema schema;
+    private final List<String> fields;
+
+    @JsonCreator
+    public PrestoIcebergPartitionSpec(
+            @JsonProperty("specId") int specId,
+            @JsonProperty("schema") PrestoIcebergSchema schema,
+            @JsonProperty("fields") List<String> fields)
+    {
+        this.specId = specId;
+        this.schema = requireNonNull(schema, "schema is null");
+        this.fields = ImmutableList.copyOf(requireNonNull(fields, "fields is null"));
+    }
+
+    @JsonProperty
+    public int getSpecId()
+    {
+        return specId;
+    }
+
+    @JsonProperty
+    public PrestoIcebergSchema getSchema()
+    {
+        return schema;
+    }
+
+    @JsonProperty
+    public List<String> getFields()
+    {
+        return fields;
+    }
+}
